0aeaf4d0b05da2da4e059cb09fa8b79e6628e2eb
jk6722 <jk6722@hanyang.ac.kr>
2023-09-28 15:37:13 +0900

    initial setting

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([]);
+
+const App = () => <RouterProvider router={router} />;
+
+export default App;
