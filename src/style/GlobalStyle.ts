bb0cde2970c45645e1e035d7700e7877ddede4d6
재국 <97966640+jk6722@users.noreply.github.com>
2023-10-28 16:39:53 +0900

    Feat/#1/design system (#6)
    * [Feat] 일부 라우팅 경로 설정 및 카카오맵 라이브러리 설치 (#3)
    
    * chore: 지도 페이지에 필요한 카카오맵 라이브러리 설치
    
    * feat: 라우팅 경로 설정
    
    * feat: 메인 페이지, 지도 페이지 디렉토리 생성
    
    * feat: 디자인 시스템 적용 및 글로벌 스타일 적용
    
    * style: StyledFonts 글자 크기 변경

---
+++ src/style/GlobalStyle.ts
@@ -0,0 +1,59 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+* {
+	margin: 0;
+	padding: 0;
+  box-sizing: border-box;
+  border-collapse: collapse;
+  text-decoration: none;
+  font-family: 'SUIT-Regular', sans-serif;
+  color: black;
+}
+
+article, aside, details, figcaption, figure, 
+footer, header, hgroup, menu, nav, section {
+	display: block;
+}
+
+body {
+	line-height: 1;
+  background: #EAF2FF;
+}
+
+ol, ul, li {
+	list-style: none;
+}
+
+blockquote, q {
+	quotes: none;
+}
+
+blockquote:before, blockquote:after,
+q:before, q:after {
+	content: '';
+	content: none;
+}
+
+
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
+}
+
+button {
+    border: none;
+    outline: none;
+    cursor: pointer;
+    background-color: transparent;
+  }
+
+  input {
+    outline: none;
+  }
+
+textarea:focus{
+	outline: none;
+}
+
+`;
