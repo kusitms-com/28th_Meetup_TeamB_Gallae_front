59c4dfe550a8a4f4f7e7259dfa034daf8c5c3032
jk6722 <jk6722@hanyang.ac.kr>
2023-11-22 17:21:39 +0900

    fix: 지도 데이터 요청 최신순으로 변경

--- src/apis/map/index.ts
+++ src/apis/map/index.ts
@@ -3,7 +3,7 @@ import Axios from '..';
 export const fetchMapMarker = () => () =>
   Axios.get('/programs/filters', {
     params: {
-      orderCriteria: '인기순',
+      orderCriteria: '최신순',
       size: 100,
     },
   });
