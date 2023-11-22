7a20434bdfae625b1d0a84904c39826410ad561f
jk6722 <jk6722@hanyang.ac.kr>
2023-11-22 17:06:04 +0900

    feat: 좋아요 버튼 API 요청 성공 확인 후 isLike 값 변경

--- src/apis/like/index.ts
+++ src/apis/like/index.ts
@@ -1,12 +1,19 @@
+import React from 'react';
 import Axios from '..';
 
-export const postLike = async (type: string, id: number) => {
+export const postLike = async (
+  type: string,
+  id: number,
+  setIsLike: React.Dispatch<React.SetStateAction<boolean>>,
+) => {
   try {
-    await Axios.post(`/favorite/${type}`, null, {
+    const res = await Axios.post(`/favorite/${type}`, null, {
       params: {
         [`${type}Id`]: id,
       },
     });
+    if (res?.data?.code === 200) setIsLike(prev => !prev);
+    return;
   } catch (e) {
     console.error(e);
   }
