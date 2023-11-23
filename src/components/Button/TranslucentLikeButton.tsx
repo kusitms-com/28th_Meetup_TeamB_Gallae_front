7ff01cf844dc41e34ee418f53dc2a3920c231ee3
jk6722 <jk6722@hanyang.ac.kr>
2023-11-23 11:27:44 +0900

    fix: 관리자는 좋아요 버튼 비활성화

--- src/components/Button/TranslucentLikeButton.tsx
+++ src/components/Button/TranslucentLikeButton.tsx
@@ -4,6 +4,8 @@ import LikeIcon from '@/assets/icons/favorite_button_icon.svg';
 import UnLikeIcon from '@/assets/icons/unfavorite_button_icon.svg';
 import { useCallback } from 'react';
 import { postLike } from '@/apis/like';
+import { useRecoilValue } from 'recoil';
+import { UserAtom } from '@/recoil/LoginAtom';
 
 interface Props {
   id: number;
@@ -12,22 +14,21 @@ interface Props {
 }
 
 const TranslucentLikeButton: React.FC<Props> = ({ id, isLike, setIsLike }) => {
+  const userInfo = useRecoilValue(UserAtom);
   // Todo: 인자로 id 전달받아서 서버에 찜 목록에 등록하는 API 연결
+
   const handleClick = useCallback(
     (e: React.MouseEvent) => {
       e.stopPropagation();
+      if (userInfo?.registNum) return;
       postLike('program', id, setIsLike);
     },
-    [isLike],
+    [isLike, userInfo.registNum],
   );
 
   return (
-    <Wrapper>
-      {isLike ? (
-        <img src={LikeIcon} onClick={handleClick} />
-      ) : (
-        <img src={UnLikeIcon} onClick={handleClick} />
-      )}
+    <Wrapper onClick={handleClick}>
+      {isLike ? <img src={LikeIcon} /> : <img src={UnLikeIcon} />}
     </Wrapper>
   );
 };
