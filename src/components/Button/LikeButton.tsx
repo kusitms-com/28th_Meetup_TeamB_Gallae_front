3e759dfd7cf240a804a518514d92366206355d35
재국 <97966640+jk6722@users.noreply.github.com>
2023-11-23 13:12:54 +0900

    Hotfix/#71/jaeguk (#79)
    * fix: refresh토큰 만료시간 6시간으로 조정
    
    * feat: 마일리지 관련 팁 데이터 변경
    
    * fix: 회원가입 완료시 confirm이 아닌 alert로 알림
    
    * fix: 내 글 목록에서 글쓰기 버튼시 Not Found로 가는 버그 해결
    
    * style: MapCard 컴포넌트 스타일링 변경
    
    * feat: 기업회원 회원가입 추가
    
    * feat: 사업자 등록번호 key registNum으로 통일
    
    * chore: 불필요한 import 제거
    
    * feat: 헤더 NavBar 클릭 시 대표 페이지로 이동
    
    * chore: D-day 표시에 D- 빠진 것 추가
    
    * chore: confrim 메시지 출력 후 취소시 뒤로가기
    
    * style: 게시물 목록 컴포넌트 width 고정
    
    * style: 게시물 Item 레이아웃 조정
    
    * style: 게시물 Item 등록날짜 태그 width 변경
    
    * fix: 게시물 상세 페이지에서 데이터 로드되지 않으면 레이아웃 보이지 않게
    
    * fix: 관리자는 좋아요 버튼 비활성화
    
    * fix: 401에러 반환시 처리하는 로직 변경
    
    * fix: 좋아요 버튼 클릭시 관리자인지 확인
    
    * fix: 관리자 좋아요 버튼 비활성화

--- src/components/Button/LikeButton.tsx
+++ src/components/Button/LikeButton.tsx
@@ -4,6 +4,8 @@ import LikeIcon from '@/assets/icons/favorite_button2_icon.svg';
 import UnLikeIcon from '@/assets/icons/unfavorite_button2_icon.svg';
 import { useCallback } from 'react';
 import { postLike } from '@/apis/like';
+import { useRecoilValue } from 'recoil';
+import { UserAtom } from '@/recoil/LoginAtom';
 
 interface Props {
   id: number;
@@ -14,21 +16,20 @@ interface Props {
 
 const LikeButton: React.FC<Props> = ({ id, isLike, setIsLike, type }) => {
   // Todo: 인자로 id 전달받아서 서버에 찜 목록에 등록하는 API 연결
+  const userInfo = useRecoilValue(UserAtom);
+
   const handleClick = useCallback(
     (e: React.MouseEvent) => {
       e.stopPropagation();
+      if (userInfo?.registNum) return;
       postLike(type, id, setIsLike);
     },
-    [isLike],
+    [isLike, userInfo.registNum],
   );
 
   return (
-    <Wrapper>
-      {isLike ? (
-        <img src={LikeIcon} onClick={handleClick} />
-      ) : (
-        <img src={UnLikeIcon} onClick={handleClick} />
-      )}
+    <Wrapper onClick={handleClick}>
+      {isLike ? <img src={LikeIcon} /> : <img src={UnLikeIcon} />}
     </Wrapper>
   );
 };
